99d0ab9a1fd4e0341a01469a81050b297ea52eca
kevokevo426-cmd <kevokevo426@gmail.com>
2026-08-24 13:40:27 +0300

    Delete app directory

--- app/resources/page.js
+++
@@ -1,9 +0,0 @@
-export default function ResourcesPage() {
-  return (
-    <main style={{ padding: "50px 20px" }}>
-      <h1>Resources</h1>
-      <p>Browse learning materials by grade and subject.</p>
-      <p>CBC, 8-4-4, KCSE past papers coming soon.</p>
-    </main>
-  );
-}
